08c822cd92dfb3a216ce5a359e94cbd00a9ad633
sir100dem <sir1001dem@gmail.com>
2015-07-29 16:17:10 +0700

    update silhouette results images

--- silhouette-detection/silhouette.py
+++ silhouette-detection/silhouette.py
@@ -2,15 +2,21 @@ import numpy as np
 import cv2
     
 def main():
-    background = cv2.imread('../images/background.jpg', 0)
-    front1 = cv2.imread('../images/front1.jpg', 0)
+    background = cv2.imread('../images/bg.png', 0)
+    front1 = cv2.imread('../images/ft.png', 0)
 
-    diff1 = front1 - background
+    # Get the different between background image an front view image
+    diff1 = background - front1
 
-    ret1,th1 = cv2.threshold(diff1,210,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    # Binary different image
+    # increase second params to reduce big salt and peppersize noise
+    blur = cv2.medianBlur(diff1,9) 
+    ret1,th1 = cv2.threshold(blur,225,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
+    # Apply the canny filter to the different
     edges1 = cv2.Canny(diff1,100,200)
 
+    # View the result
     while(1):
         cv2.imshow('image', th1)
         k = cv2.waitKey(33)
